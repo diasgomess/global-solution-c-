9066fb6564c5656f3d28da35554f4a8612c6ffae
diasgomess <matheusdiasgomes2005@gmail.com>
2025-11-12 20:38:43 -0300

    add: prompt class

---
+++ Models/Prompt.cs
@@ -0,0 +1,13 @@
+namespace BibliotecaAPI.Models
+{
+    public enum Categoria { FICCAO, TECNICO, DIDATICO }
+
+    public class Prompt
+    {
+        public string Prompt { get; set; } = string.Empty;
+        public string Titulo { get; set; } = string.Empty;
+        public string Autor { get; set; } = string.Empty;
+        public Categoria Categoria { get; set; }
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+    }
+}
